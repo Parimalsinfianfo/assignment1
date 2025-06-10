c3cd88ec4526fb03472d6ea905b2c591d2939195
Parimalsinfianfo <primaldj1009@gmail.com>
2025-06-10 07:46:10 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,25 @@
+# triangle_patterns.py
+
+def lower_triangular(n):
+    print("Lower Triangular Pattern:")
+    for i in range(1, n + 1):
+        print("* " * i)
+    print()
+
+def upper_triangular(n):
+    print("Upper Triangular Pattern:")
+    for i in range(n, 0, -1):
+        print("* " * i)
+    print()
+
+def pyramid(n):
+    print("Pyramid Pattern:")
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "* " * i)
+    print()
+
+if __name__ == "__main__":
+    rows = 5  # You can change this value
+    lower_triangular(rows)
+    upper_triangular(rows)
+    pyramid(rows)
